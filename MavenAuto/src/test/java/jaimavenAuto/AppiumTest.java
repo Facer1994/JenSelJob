5f1cc88bbaf730b0103524c5eb8492314438b68c
Facer1994 <71215087+Facer1994@users.noreply.github.com>
2022-01-23 17:20:59 +0530

    Add files via upload

---
+++ MavenAuto/src/test/java/jaimavenAuto/AppiumTest.java
@@ -0,0 +1,19 @@
+package jaimavenAuto;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeapps()
+	{
+		System.out.println("Native app Suucess");
+	}
+	
+	@Test
+	public void ios()
+	{
+		System.out.println("ios Suucess");
+	}
+
+}
---
+++ MavenAuto/src/test/java/jaimavenAuto/Restapi.java
@@ -0,0 +1,19 @@
+package jaimavenAuto;
+
+import org.testng.annotations.Test;
+
+public class Restapi {
+	
+	@Test
+	public void add()
+	{
+		System.out.println("Post Suucess");
+	}
+	
+	@Test
+	public void delete()
+	{
+		System.out.println("Delete Suucess");
+	}
+
+}
