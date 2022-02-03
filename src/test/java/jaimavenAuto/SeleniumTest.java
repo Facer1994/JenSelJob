d5a1c4636a6832f9d189449b70cc83d2217af955
Facer1994 <jaidhavanathan94@gmail.com>
2022-02-03 21:19:36 +0530

    Test Run

---
+++ src/test/java/jaimavenAuto/SeleniumTest.java
@@ -0,0 +1,19 @@
+package jaimavenAuto;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	//@Test
+	public void browserautomation()
+	{
+		System.out.println("Url launched Suucess");
+	}
+	
+	@Test
+	public void webelement()
+	{
+		System.out.println("Webelement Suucess");
+	}
+
+}
